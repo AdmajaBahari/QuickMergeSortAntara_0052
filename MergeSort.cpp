aea953e1f453a69ca858ede27ea7bf4138f6a955
AdmajaBahari <161295956+AdmajaBahari@users.noreply.github.com>
2026-07-21 14:48:25 +0700

    "Tambah loop pengisian elemen array pada input()"

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -14,7 +14,15 @@ void input() {
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+    // tambahkan di dalam input(), setelah loop while(true)
+    cout << "\n--------------------------" << endl;
+    cout << "\nInputkan Isi elemnt array" << endl;
+    cout << "\n--------------------------" << endl;
 
+    for (int i = 0; i < n; i++) {
+        cout << "Masukkan elemen ke-" << i + 1 << " : ";
+        cin >> arr[i];
+    }
 
 
 int main() {
